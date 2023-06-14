00714086fd42163ca7b6d11c38f8a1213e904894
Thien1Nguyen <tnguyen5219@gmail.com>
2023-06-14 15:01:00 -0700

    First Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,118 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using LoginReg.Models;
+using Microsoft.AspNetCore.Identity;
+namespace LoginReg.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private MyContext _context; 
+
+    public HomeController(ILogger<HomeController> logger, MyContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View("Index");
+    }
+
+    [HttpPost("register")]
+    public IActionResult Register(User newUser)
+    {
+        if(!ModelState.IsValid)
+        {
+            return View("Index");
+        }
+        // Initializing a PasswordHasher object, providing our User class as its type            
+        PasswordHasher<User> Hasher = new PasswordHasher<User>();   
+        // Updating our newUser's password to a hashed version         
+        newUser.Password = Hasher.HashPassword(newUser, newUser.Password);            
+        //Save your user object to the database 
+        _context.Add(newUser);
+        _context.SaveChanges();
+        HttpContext.Session.SetInt32("UserId", newUser.UserId);
+        return RedirectToAction("Success");
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(LoginUser userSubmission)
+    {
+        if(!ModelState.IsValid)
+        {
+            return View("Index");
+        }
+
+        User? userInDb = _context.Users.FirstOrDefault(u => u.Email == userSubmission.LoginEmail);
+        if(userInDb == null)        
+        {            
+            // Add an error to ModelState and return to View!            
+            ModelState.AddModelError("LoginEmail", "Invalid Email/Password");            
+            return View("Index");        
+        }
+        // Otherwise, we have a user, now we need to check their password                 
+        // Initialize hasher object        
+        PasswordHasher<LoginUser> hasher = new PasswordHasher<LoginUser>();                    
+        // Verify provided password against hash stored in db        
+        var result = hasher.VerifyHashedPassword(userSubmission, userInDb.Password, userSubmission.LoginPassword);
+        if(result == 0)        
+        {            
+            ModelState.AddModelError("LoginPassword", "Invalid Email/Password");            
+            return View("Index");        
+        }
+
+        HttpContext.Session.SetInt32("UserId", userInDb.UserId);
+        return RedirectToAction("Success");
+    }
+
+    [HttpGet("success")]
+    public IActionResult Success()
+    {
+        int? UserId = HttpContext.Session.GetInt32("UserId");
+        if(UserId == null)
+        {
+            return RedirectToAction("Index");
+        }
+        return View("Success");
+    }
+
+    [HttpPost("logout")]
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
+public class SessionCheckAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        // Find the session, but remember it may be null so we need int?
+        int? userId = context.HttpContext.Session.GetInt32("UserId");
+        // Check to see if we got back null
+        if(userId == null)
+        {
+            // Redirect to the Index page if there was nothing in session
+            // "Home" here is referring to "HomeController", you can use any controller that is appropriate here
+            context.Result = new RedirectToActionResult("Index", "Home", null);
+        }
+    }
+}
